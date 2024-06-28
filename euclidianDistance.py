e01faaf22669ed62fc8ae9bec215f71d1717d3a0
Mehmet Akif Ayzit <121605502+gero1nimo@users.noreply.github.com>
2024-06-28 22:25:16 +0300

    Add files via upload

--- euclidianDistance.py
+++ euclidianDistance.py
@@ -1,19 +1,17 @@
 import math as mt
-def get_tuple_from_user():
-    first_coordinate = input("İlk koordinatı aralarında ',' olacak şekilde girin: ")
-    second_coordinate = input("İkinci koordinatı aralarında ',' olacak şekilde girin: ")
-
-    first_coordinate = tuple(map(int, first_coordinate.split(',')))
-    second_coordinate = tuple(map(int, second_coordinate.split(',')))
-
-    return first_coordinate, second_coordinate
 
 def find_euclidianDistance(firstCoordinate, secondCoordinate):
-    substract_x = firstCoordinate[0] - secondCoordinate[0]
-    substract_y = firstCoordinate[1] - secondCoordinate[1]
+    return mt.sqrt((firstCoordinate[0]-secondCoordinate[0])**2 + (firstCoordinate[1]-secondCoordinate[1])**2)
 
-    return mt.sqrt(mt.pow(substract_x,2) + mt.pow(substract_y,2))
+points = [(1, 2), (4, 6), (5, 1), (3, 3)]
 
+distances = []
 
-coordinates = get_tuple_from_user()
-print(find_euclidianDistance(coordinates[0],coordinates[1]))
+for i in range(len(points)):
+    for j in range(i+1, len(points)):
+        distance = find_euclidianDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+
+print(min_distance)
